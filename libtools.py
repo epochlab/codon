b67ab6314be4e13f19247ccae70a365db0b44e4d
epochlab <james@epochlab.xyz>
2021-07-25 18:01:32 +0100

    G-C content balance

--- libtools.py
+++ libtools.py
@@ -11,7 +11,7 @@ def load(input):
 def translate(seq, dict):
     polypeptide = ""
     for i in range(0, len(seq)-3, 3):
-        codon = seq[i:i + 3].replace('T', 'U')
+        codon = seq[i:i + 3].replace('T', 'U')                                  # DNA to RNA transcription > Thymine is replace with Uracil
         amino = [k for k, v in dict.items() if codon in v]
         char = str(amino).split('/')[1].replace("']", "").strip()
         polypeptide += str(char)
--- main.py
+++ main.py
@@ -4,34 +4,40 @@ import zlib
 from libtools import load, translate
 from codon import rna_codon
 
-sars_cov_2 = ('genome/sars_cov_2.txt', (692, 1191))
-dict = rna_codon()
+SARS_CoV_2 = ('genome/SARS_CoV_2.txt', (692, 1191))
+SARS_Tor2 = ('genome/SARS_Tor2.txt', (640, 1136))
+MERS = ('genome/MERS.txt', (611, 1113))
+HIV = ('genome/HIV.txt', (454, 608))
+Ebola = ('genome/Ebola_sudan.txt', (371, 686))
 
-genome = load(sars_cov_2)
+genome = load(SARS_CoV_2)
 
-print(genome)
+# print(genome)
 print('Base Pairs:', len(genome))
+print('G-C Content:', ((genome.count('G') + genome.count('G')) / len(genome) * 100))
 print('Compression:', len(zlib.compress(genome.encode("utf-8"))))
+
+dict = rna_codon()
 print("Amino acids:", len(dict.items()))
 
 pc = translate(genome, dict)
 print(pc.split('*'))
 
-ORF1a = translate(genome[266-1: 13483], dict)                                         # ORF1a polyprotein - 4405
-ORF1b = translate(genome[13468-1: 21555], dict)                                       # ORF1b polyprotein - 2695 overlap sequence
-S = translate(genome[21563-1: 25384], dict)                                           # Spike glycoprotein (structural) - 1273
-ORF3a = translate(genome[25393-1: 26220], dict)                                       # ORF3a protein - 275
-E = translate(genome[26245-1: 26472], dict)                                           # ORF4 envelope protein (structural) - 75
-M = translate(genome[26523-1: 27191], dict)                                           # ORF5 membrane glycoprotein (structural) - 222
-ORF6 = translate(genome[27202-1: 27387], dict)                                        # ORF6 protein - 61
-ORF7a = translate(genome[27394-1: 27759], dict)                                       # ORF7a protein - 121
-ORF7b = translate(genome[27756-1: 27887], dict)                                       # ORF7b protein - 43
-ORF8 = translate(genome[27894-1: 28259], dict)                                        # ORF8 protein - 121
-N = translate(genome[28274-1: 29533], dict)                                           # ORF9 nucleocapsid phosphoprotein (structural) - 419
-ORF10 = translate(genome[29558-1: 29674], dict)                                       # ORF10 protein - 38
-
-disp = S
-print(disp, len(disp))
-
 index = "MFHLVDFQVTIAEILLIIMRTFKVSIWNLDYIINLIIKNLSKSLTENKYSQLDEEQPMEID"
 print("Search:", index, pc.count(index))
+
+# ORF1a = translate(genome[266-1: 13483], dict)                                         # ORF1a polyprotein - 4405
+# ORF1b = translate(genome[13468-1: 21555], dict)                                       # ORF1b polyprotein - 2695 overlap sequence
+# S = translate(genome[21563-1: 25384], dict)                                           # Spike glycoprotein (structural) - 1273
+# ORF3a = translate(genome[25393-1: 26220], dict)                                       # ORF3a protein - 275
+# E = translate(genome[26245-1: 26472], dict)                                           # ORF4 envelope protein (structural) - 75
+# M = translate(genome[26523-1: 27191], dict)                                           # ORF5 membrane glycoprotein (structural) - 222
+# ORF6 = translate(genome[27202-1: 27387], dict)                                        # ORF6 protein - 61
+# ORF7a = translate(genome[27394-1: 27759], dict)                                       # ORF7a protein - 121
+# ORF7b = translate(genome[27756-1: 27887], dict)                                       # ORF7b protein - 43
+# ORF8 = translate(genome[27894-1: 28259], dict)                                        # ORF8 protein - 121
+# N = translate(genome[28274-1: 29533], dict)                                           # ORF9 nucleocapsid phosphoprotein (structural) - 419
+# ORF10 = translate(genome[29558-1: 29674], dict)                                       # ORF10 protein - 38
+#
+# # disp = S
+# # print(disp, len(disp))
