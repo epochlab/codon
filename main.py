7e96c2e4d387c46113ff897babdede1e9ba3520a
epochlab <james@epochlab.xyz>
2021-11-13 02:15:23 +0000

    Amend naming convention

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ MERS = ('genome/MERS.txt', (611, 1113))
 HIV = ('genome/HIV.txt', (454, 608))
 Ebola = ('genome/Ebola.txt', (371, 686))
 
-genome = load(SARS_CoV_2)
+genome = load(Ebola)
 # print(genome)
 
 #DNA: ACGT | RNA: ACGU
@@ -27,24 +27,26 @@ dict = rna_codon()
 print("Amino acids:", len(dict.items()))
 
 pc = translate(genome, dict)
-# print(pc.split('*'))
+print(pc.split('*'))
 
-index = "MFHLVDFQVTIAEILLIIMRTFKVSIWNLDYIINLIIKNLSKSLTENKYSQLDEEQPMEID"
-print("Chain search:", index, pc.find(index))
+# >>> Explore SARS_CoV_2 - https://www.ncbi.nlm.nih.gov/nuccore/NC_045512
 
-### Identified proteins and amino acid count (SARS_CoV_2 only)
-ORF1a = translate(genome[266-1: 13483], dict)                                         # ORF1a polyprotein - 4405
-ORF1b = translate(genome[13468-1: 21555], dict)                                       # ORF1b polyprotein - 2695 overlapping sequence w/ ORF1a
-S = translate(genome[21563-1: 25384], dict)                                           # Spike glycoprotein (structural) - 1273
-ORF3a = translate(genome[25393-1: 26220], dict)                                       # ORF3a protein - 275
-E = translate(genome[26245-1: 26472], dict)                                           # ORF4 envelope protein (structural) - 75
-M = translate(genome[26523-1: 27191], dict)                                           # ORF5 membrane glycoprotein (structural) - 222
-ORF6 = translate(genome[27202-1: 27387], dict)                                        # ORF6 protein - 61
-ORF7a = translate(genome[27394-1: 27759], dict)                                       # ORF7a protein - 121
-ORF7b = translate(genome[27756-1: 27887], dict)                                       # ORF7b protein - 43
-ORF8 = translate(genome[27894-1: 28259], dict)                                        # ORF8 protein - 121
-N = translate(genome[28274-1: 29533], dict)                                           # ORF9 nucleocapsid phosphoprotein (structural) - 419
-ORF10 = translate(genome[29558-1: 29674], dict)                                       # ORF10 protein - 38
-
-disp = ORF6
-print(disp, len(disp))
+# index = "MFHLVDFQVTIAEILLIIMRTFKVSIWNLDYIINLIIKNLSKSLTENKYSQLDEEQPMEID"
+# print("Chain search:", index, pc.find(index))
+#
+# ### Identified proteins and amino acid count (SARS_CoV_2 only)
+# ORF1a = translate(genome[266-1: 13483], dict)                                         # ORF1a polyprotein - 4405
+# ORF1b = translate(genome[13468-1: 21555], dict)                                       # ORF1b polyprotein - 2695 overlapping sequence w/ ORF1a
+# S = translate(genome[21563-1: 25384], dict)                                           # Spike glycoprotein (structural) - 1273
+# ORF3a = translate(genome[25393-1: 26220], dict)                                       # ORF3a protein - 275
+# E = translate(genome[26245-1: 26472], dict)                                           # ORF4 envelope protein (structural) - 75
+# M = translate(genome[26523-1: 27191], dict)                                           # ORF5 membrane glycoprotein (structural) - 222
+# ORF6 = translate(genome[27202-1: 27387], dict)                                        # ORF6 protein - 61
+# ORF7a = translate(genome[27394-1: 27759], dict)                                       # ORF7a protein - 121
+# ORF7b = translate(genome[27756-1: 27887], dict)                                       # ORF7b protein - 43
+# ORF8 = translate(genome[27894-1: 28259], dict)                                        # ORF8 protein - 121
+# N = translate(genome[28274-1: 29533], dict)                                           # ORF9 nucleocapsid phosphoprotein (structural) - 419
+# ORF10 = translate(genome[29558-1: 29674], dict)                                       # ORF10 protein - 38
+#
+# disp = ORF6
+# print(disp, len(disp))
