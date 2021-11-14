e4b7689406f11c19614092f21dcba191f933718c
epochlab <james@epochlab.xyz>
2021-11-14 20:37:12 +0000

    def molecularweight()

--- evolve.py
+++ evolve.py
@@ -3,6 +3,10 @@
 from libtools import load, translate
 from codon import rna_table
 
+def molecularweight(acid, dict):
+    lookup = float([v for k, v in dict.items() if acid in k.split('/')[1]][0][0])
+    return lookup
+
 def score(aa):
     return
 
@@ -20,7 +24,13 @@ dict = rna_table()
 genome = load(('genome/SARS_CoV_2.txt', (692, 1191)))
 residue = translate(genome, dict).split('*')
 
+index = 12
+
 for pid, polypeptide in enumerate(residue):
-    if pid==0:
-        print("Sequence:", pid, "| Length:", len(polypeptide))
+    if pid==index:
+        mw = 0.0
+        for char in polypeptide:
+            mw += molecularweight(char, dict)
+
+        print("Sequence:", pid, "| Length:", len(polypeptide), "| Molecular Weight:", mw)
         print(polypeptide)
