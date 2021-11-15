3579d2a49eb648746ccd69a16b9fef5f6c6324db
epochlab <james@epochlab.xyz>
2021-11-15 23:42:25 +0000

    Add lookup terminus

--- evolve.py
+++ evolve.py
@@ -13,9 +13,13 @@ def lookup_halflife(acid):
     period = [v for k, v in halflife().items() if acid in k.split('/')[1]][0]
     return period
 
-def amino_count(sequence):
-    n = dict(collections.Counter(sequence))
-    return n
+def amino_count(polypeptide):
+    count = dict(collections.Counter(polypeptide))
+    return count
+
+def lookup_terminus(sequence, tid):
+    terminus = [k for k, v in codon.items() if tid in k.split('/')[1]][0]
+    return terminus
 
 def score(aa):
     return
@@ -29,27 +33,29 @@ def splice(sequence, epitope, mutant):
 def evolve(sequence, epitope):
     return
 
-genome = load(('genome/SARS_CoV_2.txt', (692, 1191)))
-residue = translate(genome, mRNA_codon()).split('*')
+codon = mRNA_codon()
 
-index = 0
+genome = load(('genome/SARS_CoV_2.txt', (692, 1191)))
+residue = translate(genome, codon).split('*')
+
+index = 5
 water_atomicweight = 18.0153
 
 for pid, polypeptide in enumerate(residue):
     if pid==index:
 
         length = len(polypeptide)
+        n_terminus = lookup_terminus(polypeptide, polypeptide[0])
+        c_terminus = lookup_terminus(polypeptide, polypeptide[-1])
 
         mw = 0.0
         for acid in polypeptide:
             mw += lookup_weight(acid)
         mw -= water_atomicweight * (length - 1)
 
-        n_terminus = polypeptide[0]
-        c_terminus = polypeptide[-1]
+        decay_rate = lookup_halflife(polypeptide[0])
 
-        decay_rate = lookup_halflife(n_terminus)
-
-        print("Sequence:", pid, "| Length:", length, "| Molecular Weight (Da):", mw, "| Half-life (N-end rule):", decay_rate)
+        print("Sequence:", pid, "| Length:", length, "| Molecular Weight (Da):", mw, "| Half-life (N-end):", decay_rate)
         print(polypeptide)
+        print("N-Terminus:", n_terminus, "| C-Terminus:", c_terminus)
         print(amino_count(polypeptide))
