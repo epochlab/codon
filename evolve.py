27124cb2e6bfb9d5bc360db9c8cd1dd8cc2bdfc1
epochlab <james@epochlab.xyz>
2021-11-16 01:55:05 +0000

    Add composition and chain search

--- evolve.py
+++ evolve.py
@@ -17,8 +17,8 @@ def lookup_halflife(acid):
     period = [v for k, v in halflife().items() if acid in k.split('/')[1]][0]
     return period
 
-def amino_count(polypeptide):
-    count = dict(collections.Counter(polypeptide))
+def amino_count(peptide):
+    count = dict(collections.Counter(peptide))
     return count
 
 def score(aa):
@@ -33,29 +33,40 @@ def splice(sequence, epitope, mutant):
 def evolve(sequence, epitope):
     return
 
+water_mass = 18.01524
+
 codon = mRNA_codon()
 
 genome = load(('genome/SARS_CoV_2.txt', (692, 1191)))
-residue = translate(genome, codon).split('*')
+residue = translate(genome, codon)
 
 index = 5
-water_atomicweight = 18.0153
-
-for pid, polypeptide in enumerate(residue):
+for pid, peptide in enumerate(residue.split('*')):
     if pid==index:
 
-        length = len(polypeptide)
-        n_terminus = lookup_acid(polypeptide[0])
-        c_terminus = lookup_acid(polypeptide[-1])
+        n_terminus = lookup_acid(peptide[0])
+        c_terminus = lookup_acid(peptide[-1])
+
+        length = len(peptide)
+
+        if length > 2 and length < 20:
+            type = "Oligopeptide"
+        else:
+            type = "Polypeptide"
 
         mw = 0.0
-        for acid in polypeptide:
+        for acid in peptide:
             mw += lookup_weight(acid)
-        mw -= water_atomicweight * (length-1)
+        mw -= water_mass * (length-1)
 
-        decay_rate = lookup_halflife(polypeptide[0])
+        decay_rate = lookup_halflife(peptide[0])
+        composition = amino_count(peptide)
 
-        print("Sequence:", pid, "| Length:", length, "| Molecular Weight (Da):", mw, "| Half-life (N-end):", decay_rate)
-        print(polypeptide)
+        print(peptide)
         print("N-Terminus:", n_terminus, "| C-Terminus:", c_terminus)
-        print(amino_count(polypeptide))
+        print("Sequence:", pid, "| Length:", length,  "| Type:", type, "| Molecular Weight (Da):", mw, "| Half-life (N-end):", decay_rate)
+        print("Chain Search:", residue.find(peptide))
+        print(composition)
+
+        for a, c in composition.items():
+            print(a + ':', "{:.3f}".format(c * (100.0/length)), "%")
--- main.py
+++ main.py
@@ -14,38 +14,32 @@ genome = load(SARS_CoV_2)
 # print(genome)
 
 #DNA: ACGT | RNA: ACGU
-print('Nucleobases:', len(genome))
-print('C-G content:', ((genome.count('C') + genome.count('G')) / len(genome) * 100), "%")
+codon = mRNA_codon()
 
-#Reading frame = START
+#Reading frame = START (ATG)
 rf = genome.find('ATG')
 if rf % 3==1:
     print('Reading frame:', rf)
 
+print('Nucleobases:', len(genome))
+print('C-G Content:', "{:.3f}".format(((genome.count('C') + genome.count('G')) / len(genome)*100)), "%")
 print('Compression:', len(zlib.compress(genome.encode("utf-8"))))
 
-dict = mRNA_codon()
-print("Amino acids:", len(dict.items()))
-
-residue = translate(genome, dict)
-print(residue.split('*'))
+residue = translate(genome, codon)
+# print(residue.split('*'))
 
 ### Identified proteins and amino acid count (SARS_CoV_2 only)
-ORF1a = translate(genome[266-1: 13483], dict)                                         # ORF1a polyprotein - 4405
-ORF1b = translate(genome[13468-1: 21555], dict)                                       # ORF1b polyprotein - 2695 overlapping sequence w/ ORF1a
-S = translate(genome[21563-1: 25384], dict)                                           # Spike glycoprotein (structural) - 1273
-ORF3a = translate(genome[25393-1: 26220], dict)                                       # ORF3a protein - 275
-E = translate(genome[26245-1: 26472], dict)                                           # ORF4 envelope protein (structural) - 75
-M = translate(genome[26523-1: 27191], dict)                                           # ORF5 membrane glycoprotein (structural) - 222
-ORF6 = translate(genome[27202-1: 27387], dict)                                        # ORF6 protein - 61
-ORF7a = translate(genome[27394-1: 27759], dict)                                       # ORF7a protein - 121
-ORF7b = translate(genome[27756-1: 27887], dict)                                       # ORF7b protein - 43
-ORF8 = translate(genome[27894-1: 28259], dict)                                        # ORF8 protein - 121
-N = translate(genome[28274-1: 29533], dict)                                           # ORF9 nucleocapsid phosphoprotein (structural) - 419
-ORF10 = translate(genome[29558-1: 29674], dict)                                       # ORF10 protein - 38
+ORF1a = translate(genome[266-1: 13483], codon)                                         # ORF1a polyprotein - 4405
+ORF1b = translate(genome[13468-1: 21555], codon)                                       # ORF1b polyprotein - 2695 overlapping sequence w/ ORF1a
+S = translate(genome[21563-1: 25384], codon)                                           # Spike glycoprotein (structural) - 1273
+ORF3a = translate(genome[25393-1: 26220], codon)                                       # ORF3a protein - 275
+E = translate(genome[26245-1: 26472], codon)                                           # ORF4 envelope protein (structural) - 75
+M = translate(genome[26523-1: 27191], codon)                                           # ORF5 membrane glycoprotein (structural) - 222
+ORF6 = translate(genome[27202-1: 27387], codon)                                        # ORF6 protein - 61
+ORF7a = translate(genome[27394-1: 27759], codon)                                       # ORF7a protein - 121
+ORF7b = translate(genome[27756-1: 27887], codon)                                       # ORF7b protein - 43
+ORF8 = translate(genome[27894-1: 28259], codon)                                        # ORF8 protein - 121
+N = translate(genome[28274-1: 29533], codon)                                           # ORF9 nucleocapsid phosphoprotein (structural) - 419
+ORF10 = translate(genome[29558-1: 29674], codon)                                       # ORF10 protein - 38
 
-disp = ORF6
-print(disp, len(disp))
-
-index = "MFHLVDFQVTIAEILLIIMRTFKVSIWNLDYIINLIIKNLSKSLTENKYSQLDEEQPMEID"
-print("Chain search:", index, residue.find(index))
+# print(ORF6)
