86201e1346c8612f3a1ffcd6fded91794ae36bae
epochlab <james@epochlab.xyz>
2021-11-17 01:42:20 +0000

    Add hydropathy index and atom dictionary

--- dict.py
+++ dict.py
@@ -82,3 +82,55 @@ def halflife():
         'STOP / *': [],
         }
     return dict
+
+def hydropathy():
+    dict = {
+        'Ala / A': [1.8],
+        'Ile / I': [4.5],
+        'Arg / R': [-4.5],
+        'Leu / L': [3.8],
+        'Asn / N': [-3.5],
+        'Lys / K': [-3.9],
+        'Asp / D': [-3.5],
+        'Met / M': [1.9],
+        'Phe / F': [2.8],
+        'Cys / C': [2.5],
+        'Pro / P': [-1.6],
+        'Gln / Q': [-3.5],
+        'Ser / S': [-0.8],
+        'Glu / E': [-3.5],
+        'Thr / T': [-0.7],
+        'Trp / W': [-0.9],
+        'Gly / G': [-0.4],
+        'Tyr / Y': [-1.3],
+        'His / H': [-3.2],
+        'Val / V': [4.2],
+        'STOP / *': [],
+        }
+    return dict
+
+def atom():
+    dict = {
+        'Ala / A': {C: 3, H: 7 , O: 2, N: 1, S: 0}, # C3H7NO2
+        'Ile / I': {C: 6, H: 13, O: 2, N: 1, S: 0}, # C6H13NO2
+        'Arg / R': {C: 6, H: 14, O: 2, N: 4, S: 0}, # C6H14N4O2
+        'Leu / L': {C: 6, H: 13, O: 2, N: 1, S: 0}, # C6H13NO2
+        'Asn / N': {C: 4, H: 8 , O: 3, N: 2, S: 0}, # C4H8N2O3
+        'Lys / K': {C: 6, H: 14, O: 2, N: 2, S: 0}, # C6H14N2O2
+        'Asp / D': {C: 4, H: 7 , O: 4, N: 1, S: 0}, # C4H7NO4
+        'Met / M': {C: 5, H: 11 ,O: 2, N: 1, S: 1}, # C5H11NO2S
+        'Phe / F': {C: 9, H: 11, O: 2, N: 1, S: 0}, # C9H11NO2
+        'Cys / C': {C: 3, H: 7 , O: 2, N: 1, S: 1}, # C3H7NO2S
+        'Pro / P': {C: 5, H: 9 , O: 2, N: 1, S: 0}, # C5H9NO2
+        'Gln / Q': {C: 5, H: 10, O: 3, N: 2, S: 0}, # C5H10N2O3
+        'Ser / S': {C: 3, H: 7 , O: 3, N: 1, S: 0}, # C3H7NO3
+        'Glu / E': {C: 5, H: 9 , O: 4, N: 1, S: 0}, # C5H9NO4
+        'Thr / T': {C: 4, H: 9 , O: 3, N: 1, S: 0}, # C4H9NO3
+        'Trp / W': {C: 11,H: 12, O: 2, N: 2, S: 0}, # C11H12N2O2
+        'Gly / G': {C: 2, H: 5 , O: 2, N: 1, S: 0}, # C2H5NO2
+        'Tyr / Y': {C: 9, H: 11, O: 3, N: 1, S: 0}, # C9H11NO3
+        'His / H': {C: 6, H: 9 , O: 2, N: 3, S: 0}, # C6H9N3O2
+        'Val / V': {C: 5, H: 11, O: 2, N: 1, S: 0}, # C5H11NO2
+        'STOP / *': [],
+        }
+    return dict
--- evolve.py
+++ evolve.py
@@ -3,23 +3,34 @@
 import collections
 
 from libtools import load, translate
-from dict import mRNA_codon, molecular_weight, halflife
+from dict import mRNA_codon, molecular_weight, halflife, hydropathy
 
 def lookup_acid(acid):
     terminus = [k for k, v in codon.items() if acid in k.split('/')[1]][0]
     return terminus
 
-def lookup_weight(acid):
-    weight = [v for k, v in molecular_weight().items() if acid in k.split('/')[1]][0][0]
+def amino_count(peptide):
+    count = dict(collections.Counter(peptide))
+    return count
+
+def lookup_weight(peptide):
+    water_mass = 18.01524
+    weight = 0
+    for i in peptide:
+        weight += [v for k, v in molecular_weight().items() if i in k.split('/')[1]][0][0]
+    weight -= water_mass * (len(peptide)-1)
     return weight
 
 def lookup_halflife(acid):
     period = [v for k, v in halflife().items() if acid in k.split('/')[1]][0]
     return period
 
-def amino_count(peptide):
-    count = dict(collections.Counter(peptide))
-    return count
+def hydropathy_index(peptide):
+    index = 0
+    for i in peptide:
+         index += float([v for k, v in hydropathy().items() if i in k.split('/')[1]][0][0])
+    index /= len(peptide)
+    return index
 
 def score(aa):
     return
@@ -37,9 +48,7 @@ genome = load(('genome/SARS_CoV_2.txt', (692, 1191)))
 codon = mRNA_codon()
 residue = translate(genome, codon)
 
-water_mass = 18.01524
-
-index = 5
+index = 12
 for pid, peptide in enumerate(residue.split('*')):
     if pid==index:
 
@@ -53,19 +62,16 @@ for pid, peptide in enumerate(residue.split('*')):
         else:
             type = "Polypeptide"
 
-        mw = 0.0
-        for acid in peptide:
-            mw += lookup_weight(acid)
-        mw -= water_mass * (length-1)
-
+        mw = lookup_weight(peptide)
+        hp = hydropathy_index(peptide)
         decay_rate = lookup_halflife(peptide[0])
         composition = amino_count(peptide)
 
         print(peptide)
         print("N-Terminus:", n_terminus, "| C-Terminus:", c_terminus)
         print("Sequence:", pid, "| Length:", length,  "| Type:", type, "| Molecular Weight (Da):", mw, "| Half-life (N-end):", decay_rate)
+        print("Grand average of hydropathicity:", hp)
         print("Chain Search:", residue.find(peptide))
         print(composition)
-
         for a, c in composition.items():
             print(a, c * (100.0/length), "%")
