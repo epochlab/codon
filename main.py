d7e9ade3ea54e765a8985e12339503ddf47f6abc
James Roberts <James@MacBook-Air.local>
2022-03-11 22:18:48 +0000

    Amend reading_frame() and rename codon > codon_table

--- main.py
+++ main.py
@@ -14,33 +14,33 @@ Ebola = ('genome/Ebola.txt', (371, 686))
 genome = load(SARS_CoV_2)
 # print(genome)
 
-codon = mRNA_codon()
-
-#Reading frame = START (ATG)
-rf = genome.find('ATG')
-if rf % 3==1:
-    print('Reading frame:', rf)
+codon_table = mRNA_codon()
 
 print('Nucleobases:', len(genome))
 print('C-G Content:', round((genome.count('C') + genome.count('G')) / len(genome)*100, 3), "%")
 print('Compression:', len(zlib.compress(genome.encode("utf-8"))))
+print('[START] Frame:', reading_frame(genome))
 
-residue = translate(genome, codon)
-print(residue.split('*'))
+# Sequential CGG position - Doesn't not align with a modulo of 3.
+print('CpG:', genome.find('CGGCGG'))
 
-# >>> Identified proteins and amino acid count (SARS_CoV_2 only)
-ORF1a = translate(genome[266-1: 13483], codon)                                         # ORF1a polyprotein - 4405
-ORF1b = translate(genome[13468-1: 21555], codon)                                       # ORF1b polyprotein - 2695 overlapping sequence w/ ORF1a
-S = translate(genome[21563-1: 25384], codon)                                           # Spike glycoprotein (structural) - 1273
-ORF3a = translate(genome[25393-1: 26220], codon)                                       # ORF3a protein - 275
-E = translate(genome[26245-1: 26472], codon)                                           # ORF4 envelope protein (structural) - 75
-M = translate(genome[26523-1: 27191], codon)                                           # ORF5 membrane glycoprotein (structural) - 222
-ORF6 = translate(genome[27202-1: 27387], codon)                                        # ORF6 protein - 61
-ORF7a = translate(genome[27394-1: 27759], codon)                                       # ORF7a protein - 121
-ORF7b = translate(genome[27756-1: 27887], codon)                                       # ORF7b protein - 43
-ORF8 = translate(genome[27894-1: 28259], codon)                                        # ORF8 protein - 121
-N = translate(genome[28274-1: 29533], codon)                                           # ORF9 nucleocapsid phosphoprotein (structural) - 419
-ORF10 = translate(genome[29558-1: 29674], codon)                                       # ORF10 protein - 38
+residue = translate(genome, codon_table)
+# print(residue.split('*'))
+
+# Identified proteins and amino acid count (SARS_CoV_2 only)
+# ORF = Open Reading Frame
+ORF1a = translate(genome[266-1: 13483], codon_table)                                         # ORF1a polyprotein - 4405
+ORF1b = translate(genome[13468-1: 21555], codon_table)                                       # ORF1b polyprotein - 2695 overlapping sequence w/ ORF1a
+S = translate(genome[21563-1: 25384], codon_table)                                           # Spike glycoprotein (structural) - 1273
+ORF3a = translate(genome[25393-1: 26220], codon_table)                                       # ORF3a protein - 275
+E = translate(genome[26245-1: 26472], codon_table)                                           # ORF4 envelope protein (structural) - 75
+M = translate(genome[26523-1: 27191], codon_table)                                           # ORF5 membrane glycoprotein (structural) - 222
+ORF6 = translate(genome[27202-1: 27387], codon_table)                                        # ORF6 protein - 61
+ORF7a = translate(genome[27394-1: 27759], codon_table)                                       # ORF7a protein - 121
+ORF7b = translate(genome[27756-1: 27887], codon_table)                                       # ORF7b protein - 43
+ORF8 = translate(genome[27894-1: 28259], codon_table)                                        # ORF8 protein - 121
+N = translate(genome[28274-1: 29533], codon_table)                                           # ORF9 nucleocapsid phosphoprotein (structural) - 419
+ORF10 = translate(genome[29558-1: 29674], codon_table)                                       # ORF10 protein - 38
 # print(ORF6)
 
 index = 0
