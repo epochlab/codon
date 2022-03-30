2055760c74b22e93a0fba82e491275e428b197ec
epochlab <james@epochlab.xyz>
2022-03-30 18:51:18 +0100

    Clean up

--- download_fasta.py
+++ download_fasta.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
-import requests
+import requests, argparse, sys
 
-UID = "NC_045512.2"
+parser = argparse.ArgumentParser()
+parser.add_argument('-uid', type=str, default='NC_045512.2')
+args = parser.parse_args(sys.argv[1:])
+
+UID = args.uid
+
 content = requests.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id={fasta}&rettype=fasta".format(fasta=UID))
 content.raise_for_status()
 
@@ -10,4 +15,5 @@ filename = "data/" + UID + ".txt"
 
 with open(filename, 'w') as f:
     f.write(content.text)
-    print("Download complete", UID)
+
+print("Download complete", UID)
