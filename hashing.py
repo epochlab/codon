aa4bc736800a68de4ebc317dd3c47cee7b1dcaa9
epochlab <james@epochlab.xyz>
2022-04-23 00:38:08 +0100

    seq_to_pixels function

--- hashing.py
+++ hashing.py
@@ -17,30 +17,33 @@ def average_hash(image, hash_size=8, mean=np.mean):
     diff = pixels > mean(pixels)
     return binary_array_to_hex(diff)
 
+def seq_to_pixels(seq):
+    pixels = []
+
+    for n in genome:
+        if n == 'A':
+            pixels.append((10,10,10))
+        if n == 'C':
+            pixels.append((255,255,255))
+        if n == 'G':
+            pixels.append((65,150,65))
+        if n == 'T':
+            pixels.append((0,50,140))
+
+    array = np.array(pixels, dtype=np.uint8)
+
+    width = round(math.sqrt(len(genome)))
+    height = len(genome) // width
+
+    img = Image.frombytes("RGB", (width, height), bytes(array))
+    return img
+
 UID = 'NC_001474.2'
 label, genome = load('genome/' + UID + '.fasta')
 
-pixels = []
-
-for n in genome:
-    if n == 'A':
-        pixels.append((10,10,10))
-    if n == 'C':
-        pixels.append((255,255,255))
-    if n == 'G':
-        pixels.append((65,150,65))
-    if n == 'T':
-        pixels.append((0,50,140))
-
-array = np.array(pixels, dtype=np.uint8)
-
-width = round(math.sqrt(len(genome)))
-height = len(genome) // width
-
-img = Image.frombytes("RGB", (width, height), bytes(array))
-
+img = seq_to_pixels(genome)
 hash = average_hash(img)
 
-img.save(UID + "_" + hash + '.png')
-
 print("Average Hash:", hash)
+
+img.save(UID + "_" + hash + '.png')
