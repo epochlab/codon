fba80b6ae8c5b31800abbec1bdbac7180dacb29d
epochlab <james@epochlab.xyz>
2022-04-23 20:32:27 +0100

    Annotations

--- libtools.py
+++ libtools.py
@@ -20,11 +20,11 @@ def translate(seq, dict):
         codon = seq[i:i + 3].replace('T', 'U')                                  # DNA to RNA transcription - Thymine is replaced with Uracil.
         amino = [k for k, v in dict.items() if codon in v]
 
-        if codon=='AUG':                                                  # START open reading frame
+        if codon=='AUG':                                                        # START open reading frame
             count = 3
         if count==3 and len(codon)==3:
             res += str(amino).split('/')[1].replace("']", "").strip()
-        if codon=='UAG' or codon=='UAA' or codon=='UGA':              # STOP open reading frame
+        if codon=='UAG' or codon=='UAA' or codon=='UGA':                         # STOP open reading frame
             i += 2
             count = 1
 
